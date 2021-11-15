7e9f531417792f857de2274c4111e99726b3f901
GuillaumeBlanchetApple <48529373+GuillaumeBlanchetApple@users.noreply.github.com>
2021-11-15 16:50:31 +0100

    update to Swift 5.3 with content

--- Package.swift
+++ Package.swift
@@ -3,5 +3,11 @@ import PackageDescription
 
 let package = Package(
 	name: "JTMaterialSpinner",
+    platforms: [.iOS(.v10)],
+    products: [.library(name: "JTMaterialSpinner", targets: ["JTMaterialSpinner"])],
+    targets: [
+        .target(name: "JTMaterialSpinner", path: "JTMaterialSpinner")
+    ],
     swiftLanguageVersions: [SwiftVersion.v5]
+    
 )
